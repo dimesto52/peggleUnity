a8a6a708d373451663241434cbb4c285532a0644
dimesto52 <98359317+dimesto52@users.noreply.github.com>
2023-01-11 14:43:54 +0100

    ep 04 bounce and bucket move

---
+++ Assets/bucketMove.cs
@@ -0,0 +1,42 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class bucketMove : MonoBehaviour
+{
+    public float speed = 1.0f;
+    public float min = -3.0f;
+    public float max = 3.0f;
+
+    bool doRight = true;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (doRight)
+        {
+            transform.position += Vector3.right * Time.deltaTime * speed;
+            if (transform.position.x > max)
+            {
+                transform.position.Set(max, transform.position.y, 0);
+                doRight = !doRight;
+            }
+        }
+        else
+        {
+            transform.position -= Vector3.right * Time.deltaTime * speed;
+            if (transform.position.x < min)
+            {
+                transform.position.Set(max, transform.position.y, 0);
+                doRight = !doRight;
+            }
+        }
+
+    }
+}
